985ff591ff4a190860e7eaf1f43b9231d228d7c6
Erik Merkle <erik.merkle@datastax.com>
2021-01-15 11:18:45 -0600

    fix Dockerfiles for 3.x/4.x agents

--- management-api-shim-4.x/src/main/java/org/apache/cassandra/transport/UnixSocketServer4x.java
+++ management-api-shim-4.x/src/main/java/org/apache/cassandra/transport/UnixSocketServer4x.java
@@ -5,42 +5,56 @@
  */
 package org.apache.cassandra.transport;
 
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
+import com.datastax.mgmtapi.ipc.IPCController;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
+import io.netty.channel.ChannelPromise;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.VoidChannelPromise;
+import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.util.Attribute;
 import org.apache.cassandra.auth.IAuthenticator;
+import org.apache.cassandra.cql3.QueryProcessor;
 import org.apache.cassandra.service.ClientState;
 import org.apache.cassandra.service.ClientWarn;
 import org.apache.cassandra.service.QueryState;
 import org.apache.cassandra.transport.messages.AuthenticateMessage;
 import org.apache.cassandra.transport.messages.ErrorMessage;
 import org.apache.cassandra.transport.messages.ReadyMessage;
+import org.apache.cassandra.transport.messages.StartupMessage;
+import org.apache.cassandra.transport.messages.SupportedMessage;
 import org.apache.cassandra.utils.JVMStabilityInspector;
 
 public class UnixSocketServer4x
 {
-    private static final Logger logger = LoggerFactory.getLogger(UnixSocketServer4x.class);
+    private static final Logger logger = LoggerFactory.getLogger(IPCController.class);
+
+    // Names of handlers used in pre-V5 pipelines
+    private static final String ENVELOPE_DECODER          = "envelopeDecoder";
+    private static final String ENVELOPE_ENCODER          = "envelopeEncoder";
+    private static final String MESSAGE_DECOMPRESSOR      = "decompressor";
+    private static final String MESSAGE_COMPRESSOR        = "compressor";
+    private static final String MESSAGE_DECODER           = "messageDecoder";
+    private static final String MESSAGE_ENCODER           = "messageEncoder";
+    private static final String LEGACY_MESSAGE_PROCESSOR  = "legacyCqlProcessor";
+    private static final String INITIAL_HANDLER           = "initialHandler";
+    private static final String EXCEPTION_HANDLER         = "exceptionHandler";
 
     public static ChannelInitializer<Channel> makeSocketInitializer(final Server.ConnectionTracker connectionTracker)
     {
-        // Stateless handlers
-        final PreV5Handlers.ProtocolDecoder messageDecoder = PreV5Handlers.ProtocolDecoder.instance;
-        final PreV5Handlers.ProtocolEncoder messageEncoder = PreV5Handlers.ProtocolEncoder.instance;
-        final ChannelHandler frameDecompressor = Envelope.Decompressor.instance;
-        final ChannelHandler frameCompressor = Envelope.Compressor.instance;
-        final Envelope.Encoder frameEncoder = Envelope.Encoder.instance;
-        final PreV5Handlers.ExceptionHandler exceptionHandler = PreV5Handlers.ExceptionHandler.instance;
-        final UnixSockMessage dispatcher = new UnixSockMessage();
-
+        logger.info("Creating Channel Initializer");
         return new ChannelInitializer<Channel>()
         {
             @Override
@@ -48,34 +62,22 @@ public class UnixSocketServer4x
             {
                 ChannelPipeline pipeline = channel.pipeline();
 
-                // OLD way
-                // pipeline.addLast("frameDecoder", new Frame.Decoder((channel1, version) ->
-                //       new UnixSocketConnection(channel1, version, connectionTracker)));
-
-                // TODO: This bit needs to change to conform to the new Frame -> Envelope changes
-                // in CASSANDRA-15299. Envelope.Decoder() does not take a ConnectionFactory instance
-                // like Frame.Decoder() did, so we'll have to figure out how to adjust this to use
-                // the new PipelineConfigurator introduced in CASSANDRA-15299.
-
-                // NEW way, but isn't going to work as is.
-                pipeline.addLast("frameDecoder", new Envelope.Decoder());
-                pipeline.addLast("frameEncoder", frameEncoder);
-
-                pipeline.addLast("frameDecompressor", frameDecompressor);
-                pipeline.addLast("frameCompressor", frameCompressor);
-
-                pipeline.addLast("messageDecoder", messageDecoder);
-                pipeline.addLast("messageEncoder", messageEncoder);
-
-                pipeline.addLast("executor", dispatcher);
-
+                pipeline.addLast(ENVELOPE_ENCODER, Envelope.Encoder.instance);
+                pipeline.addLast(INITIAL_HANDLER,
+                        new PipelineChannelInitializer(
+                                new Envelope.Decoder(),
+                                (channel1, version) ->
+                                        new UnixSocketConnection(
+                                                channel1,
+                                                version,
+                                                connectionTracker)));
                 // The exceptionHandler will take care of handling exceptionCaught(...) events while still running
                 // on the same EventLoop as all previous added handlers in the pipeline. This is important as the used
                 // eventExecutorGroup may not enforce strict ordering for channel events.
                 // As the exceptionHandler runs in the EventLoop as the previous handlers we are sure all exceptions are
                 // correctly handled before the handler itself is removed.
                 // See https://issues.apache.org/jira/browse/CASSANDRA-13649
-                pipeline.addLast("exceptionHandler", exceptionHandler);
+                pipeline.addLast(EXCEPTION_HANDLER, PreV5Handlers.ExceptionHandler.instance);
             }
         };
     }
@@ -135,7 +137,7 @@ public class UnixSocketServer4x
 
         private final ClientState clientState;
         private volatile State state;
-        private final ConcurrentMap<Integer, QueryState> queryStates = new ConcurrentHashMap<>();
+        // private final ConcurrentMap<Integer, QueryState> queryStates = new ConcurrentHashMap<>();
 
         public UnixSocketConnection(Channel channel, ProtocolVersion version, Connection.Tracker tracker)
         {
@@ -144,6 +146,7 @@ public class UnixSocketServer4x
             this.state = State.UNINITIALIZED;
         }
 
+        @Override
         public QueryState validateNewMessage(Message.Type type, ProtocolVersion version)
         {
             return validateNewMessage(type, version, -1);
@@ -203,9 +206,137 @@ public class UnixSocketServer4x
             }
         }
 
+        @Override
         public IAuthenticator.SaslNegotiator getSaslNegotiator(QueryState queryState)
         {
             return null;
+        }
+    }
+
+    static class PipelineChannelInitializer extends ByteToMessageDecoder
+    {
+        Envelope.Decoder decoder;
+        Connection.Factory factory;
+
+        PipelineChannelInitializer(Envelope.Decoder decoder, Connection.Factory factory)
+        {
+            this.decoder = decoder;
+            this.factory = factory;
+        }
+
+        @Override
+        protected void decode(ChannelHandlerContext ctx, ByteBuf buffer, List<Object> out) throws Exception {
+            Envelope inbound = decoder.decode(buffer);
+            if (inbound == null)
+                return;
+
+            try
+            {
+                Envelope outbound;
+                switch (inbound.header.type)
+                {
+                    case OPTIONS:
+                        logger.debug("OPTIONS received {}", inbound.header.version);
+                        List<String> cqlVersions = new ArrayList<>();
+                        cqlVersions.add(QueryProcessor.CQL_VERSION.toString());
+
+                        List<String> compressions = new ArrayList<>();
+                        if (Compressor.SnappyCompressor.instance != null)
+                            compressions.add("snappy");
+                        // LZ4 is always available since worst case scenario it default to a pure JAVA implem.
+                        compressions.add("lz4");
+
+                        Map<String, List<String>> supportedOptions = new HashMap<>();
+                        supportedOptions.put(StartupMessage.CQL_VERSION, cqlVersions);
+                        supportedOptions.put(StartupMessage.COMPRESSION, compressions);
+                        supportedOptions.put(StartupMessage.PROTOCOL_VERSIONS, ProtocolVersion.supportedVersions());
+                        SupportedMessage supported = new SupportedMessage(supportedOptions);
+                        outbound = supported.encode(inbound.header.version);
+                        ctx.writeAndFlush(outbound);
+                        break;
+
+                    case STARTUP:
+                        Attribute<Connection> attrConn = ctx.channel().attr(Connection.attributeKey);
+                        Connection connection = attrConn.get();
+                        if (connection == null)
+                        {
+                            connection = factory.newConnection(ctx.channel(), inbound.header.version);
+                            attrConn.set(connection);
+                        }
+                        assert connection instanceof ServerConnection;
+
+                        StartupMessage startup = (StartupMessage) Message.Decoder.decodeMessage(ctx.channel(), inbound);
+                        // InetAddress remoteAddress = ((InetSocketAddress) ctx.channel().remoteAddress()).getAddress();
+                        // final ClientResourceLimits.Allocator allocator = ClientResourceLimits.getAllocatorForEndpoint(remoteAddress);
+
+                        ChannelPromise promise;
+                        if (inbound.header.version.isGreaterOrEqualTo(ProtocolVersion.V5))
+                        {
+                            // v5 not yet supported
+                            logger.warn("PROTOCOL v5 not yet supported.");
+
+                            // in this case we need to defer configuring the pipeline until after the response
+                            // has been sent, as the frame encoding specified in v5 should not be applied to
+                            // the STARTUP response.
+//                            allocator.allocate(inbound.header.bodySizeInBytes);
+//                            promise = AsyncChannelPromise.withListener(ctx, future -> {
+//                                if (future.isSuccess())
+//                                {
+//                                    logger.debug("Response to STARTUP sent, configuring pipeline for {}", inbound.header.version);
+//                                    configurator.configureModernPipeline(ctx, allocator, inbound.header.version, startup.options);
+//                                    allocator.release(inbound.header.bodySizeInBytes);
+//                                }
+//                                else
+//                                {
+//                                    Throwable cause = future.cause();
+//                                    if (null == cause)
+//                                        cause = new ServerError("Unexpected error establishing connection");
+//                                    logger.warn("Writing response to STARTUP failed, unable to configure pipeline", cause);
+//                                    ErrorMessage error = ErrorMessage.fromException(cause);
+//                                    Envelope response = error.encode(inbound.header.version);
+//                                    ChannelPromise closeChannel = AsyncChannelPromise.withListener(ctx, f -> ctx.close());
+//                                    ctx.writeAndFlush(response, closeChannel);
+//                                    if (ctx.channel().isOpen())
+//                                        ctx.channel().close();
+//                                }
+//                            });
+                        }
+                        // no need to configure the pipeline asynchronously in this case
+                        // the capacity obtained from allocator for the STARTUP message
+                        // is released when flushed by the legacy dispatcher/flusher so
+                        // there's no need to explicitly release that here either.
+
+                        ChannelPipeline pipeline = ctx.channel().pipeline();
+                        pipeline.addBefore(ENVELOPE_ENCODER, ENVELOPE_DECODER, new Envelope.Decoder());
+                        pipeline.addBefore(INITIAL_HANDLER, MESSAGE_DECOMPRESSOR, Envelope.Decompressor.instance);
+                        pipeline.addBefore(INITIAL_HANDLER, MESSAGE_COMPRESSOR, Envelope.Compressor.instance);
+                        pipeline.addBefore(INITIAL_HANDLER, MESSAGE_DECODER, PreV5Handlers.ProtocolDecoder.instance);
+                        pipeline.addBefore(INITIAL_HANDLER, MESSAGE_ENCODER, PreV5Handlers.ProtocolEncoder.instance);
+                        pipeline.addBefore(INITIAL_HANDLER, LEGACY_MESSAGE_PROCESSOR, new UnixSockMessage());
+                        pipeline.remove(INITIAL_HANDLER);
+
+
+                        promise = new VoidChannelPromise(ctx.channel(), false);
+
+                        final Message.Response response = Dispatcher.processRequest((ServerConnection) connection, startup);
+                        outbound = response.encode(inbound.header.version);
+                        ctx.writeAndFlush(outbound, promise);
+                        logger.debug("Configured pipeline: {}", ctx.pipeline());
+                        break;
+
+                    default:
+                        ErrorMessage error =
+                            ErrorMessage.fromException(
+                                new ProtocolException(String.format("Unexpected message %s, expecting STARTUP or OPTIONS",
+                                                                    inbound.header.type)));
+                        outbound = error.encode(inbound.header.version);
+                        ctx.writeAndFlush(outbound);
+                }
+            }
+            finally
+            {
+                inbound.release();
+            }
         }
     }
 }
